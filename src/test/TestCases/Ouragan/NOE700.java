4b6e9d09408a0169533df077d693831e7b86fe96
ValentinVANHAUTE <vvanhaute@norauto.com>
2021-01-13 16:24:54 +0100

    Test proxy

--- src/test/TestCases/Ouragan/NOE700.java
+++ src/test/TestCases/Ouragan/NOE700.java
@@ -13,7 +13,7 @@ public class NOE700 extends BaseTest {
     @Parameters({"jsonFilePath","discountLabel","discountAmount","expectedTotal"})
     @Test(description = "Scanner un RA contenant une remise manuelle")
     public void noe700(String jsonFilePath, String discountLabel, String discountAmount, String expectedTotal) throws IOException, InterruptedException {
-        System.setProperty("java.net.useSystemProxies","true");
+        //System.setProperty("java.net.useSystemProxies","true");
         /*System.setProperty("https.proxyHost", "Panoz4.nor.lan");
         System.setProperty("https.proxyPort", "8080");
         System.setProperty("https.proxyUser", "vvanhaute");
