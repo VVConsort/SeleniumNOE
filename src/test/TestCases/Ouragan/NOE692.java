c91aba56084d661d96f0b2e33bbe087814c44b54
ValentinVANHAUTE <vvanhaute@norauto.com>
2021-01-13 11:54:10 +0100

    Propriété TS Ouragan

---
+++ src/test/TestCases/Ouragan/NOE692.java
@@ -0,0 +1,33 @@
+package TestCases.Ouragan;
+
+import Helpers.Test.BaseTest;
+import Step.LoggingStep;
+import Step.OuraganStep;
+import Step.ScanStep;
+import Step.TicketStep;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class NOE692 extends BaseTest {
+
+    @Parameters({"jsonFilePath","expectedPendingAmount"})
+    @Test(description = "Part : Récupérer une commande Click and Collect")
+    public void noe692(String jsonFilePath, String expectedPendingAmount) throws IOException, InterruptedException {
+        // Envoie du relevé atelier vers OB
+        String documentCode = OuraganStep.postWorkOrderToOpenBravo(jsonFilePath);
+        // Lancement et log sur OB
+        currentDriver = LoggingStep.launchAndLogOB();
+        // Vidage du ticket
+        TicketStep.deleteTicket(currentDriver);
+        // Scan du BT
+        ScanStep.scanValue(documentCode, currentDriver);
+        // Fermeture des documents associés
+        TicketStep.closeMergedDocuments(currentDriver);
+        // Vérification du montant à payer
+        TicketStep.checkTotalToPay(newStepValue(expectedPendingAmount,false));
+        // Vidage du ticket
+        TicketStep.deleteTicket(currentDriver);
+    }
+}
---
+++ src/test/TestCases/Ouragan/NOE800.java
@@ -0,0 +1,25 @@
+package TestCases.Ouragan;
+
+import Helpers.Test.BaseTest;
+import Step.LoggingStep;
+import Step.OuraganStep;
+import Step.ScanStep;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class NOE800 extends BaseTest {
+
+    @Parameters({"jsonFilePath"})
+    @Test(description = "Remise immédiate : Montage des pneus offert pour l'achat de 2 pneus")
+    public void noe800(String jsonFilePath) throws IOException, InterruptedException {
+        // Envoie du relevé atelier vers OB
+        //String documentCode = OuraganStep.postWorkOrderToOpenBravo(jsonFilePath);
+        // Lancement et log sur OB
+        currentDriver = LoggingStep.launchAndLogOB();
+        // Ouverture du BT intégré
+        ScanStep.scanValue("0003001455771320881",currentDriver);
+        System.out.println("002280016286111023517");
+    }
+}
