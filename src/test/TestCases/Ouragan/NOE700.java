36eb69bb425b5fd7b6b541ec6f25a59b73c9288f
ValentinVANHAUTE <vvanhaute@norauto.com>
2021-01-13 16:31:22 +0100

    Test proxy

--- src/test/TestCases/Ouragan/NOE700.java
+++ src/test/TestCases/Ouragan/NOE700.java
@@ -13,11 +13,6 @@ public class NOE700 extends BaseTest {
     @Parameters({"jsonFilePath","discountLabel","discountAmount","expectedTotal"})
     @Test(description = "Scanner un RA contenant une remise manuelle")
     public void noe700(String jsonFilePath, String discountLabel, String discountAmount, String expectedTotal) throws IOException, InterruptedException {
-        //System.setProperty("java.net.useSystemProxies","true");
-        /*System.setProperty("https.proxyHost", "Panoz4.nor.lan");
-        System.setProperty("https.proxyPort", "8080");
-        System.setProperty("https.proxyUser", "vvanhaute");
-        System.setProperty("https.proxyPassword", "SCEpter1986");*/
         // Envoie du relevé atelier vers OB
         String documentCode = OuraganStep.postWorkOrderToOpenBravo(jsonFilePath);
         // Lancement et log sur OB
