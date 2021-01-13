c6212217804e6052b6ba887fe04d11cb85410198
ValentinVANHAUTE <vvanhaute@norauto.com>
2021-01-13 15:42:59 +0100

    Test proxy

--- src/main/View/OpenBravoLauncher.java
+++ src/main/View/OpenBravoLauncher.java
@@ -23,10 +23,11 @@ public class OpenBravoLauncher {
         String pathToChromeDriver = "src/main/resources/chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
         // Proxy
-        System.setProperty("https.proxyHost", "Panoz4.nor.lan");
+        System.setProperty("java.net.useSystemProxies","true");
+        /*System.setProperty("https.proxyHost", "Panoz4.nor.lan");
         System.setProperty("https.proxyPort", "8080");
 
-        /*System.setProperty("https.proxyUser", "vvanhaute");
+        System.setProperty("https.proxyUser", "vvanhaute");
         System.setProperty("https.proxyPassword", "SCEpter1986");*/
 
         // Chemin vers le profil contenant le cache OB
