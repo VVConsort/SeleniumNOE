bbe39a60f6eda48b20b5da2307e04c22cb3cf3d9
ValentinVANHAUTE <vvanhaute@norauto.com>
2021-01-18 14:26:55 +0100

    Vue envoie ticket

---
+++ src/main/Enums/SendTicketMode.java
@@ -0,0 +1,16 @@
+package Enums;
+
+public enum SendTicketMode {
+    MAIL_ONLY("Email seulement");
+
+    // Label du mode d'envoi
+    private String label;
+
+    SendTicketMode(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
---
+++ src/main/View/Ticket/Payment/SendTicketView.java
@@ -0,0 +1,23 @@
+package View.Ticket.Payment;
+
+import View.BaseView;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SendTicketView extends BaseView {
+    // Boutton "Email seulement"
+    @FindBy(xpath = "//*[@id=\"terminal_containerWindow_pointOfSale_obsmail_getEmailAddressModal_emailAddressModal__btnEmailOnly\"]")
+    private WebElement mailOnlyBtn;
+
+    public SendTicketView(WebDriver driver) {
+        init(driver, this);
+    }
+
+    /**
+     * Click sur le bouton "Email seulement"
+     */
+    public void clickMailOnly() {
+        super.click(mailOnlyBtn);
+    }
+}
