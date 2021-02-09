cec829c40e853cd9c1a4e972b113038b43690eec
ValentinVANHAUTE <vvanhaute@norauto.com>
2021-02-09 11:35:04 +0100

    NOE629-> Ajout dans la TS NOE651

---
+++ src/test/TestCases/Ouragan/NOE651.java
@@ -0,0 +1,45 @@
+package TestCases.Ouragan;
+
+import Enums.SendTicketMode;
+import Helpers.Test.BaseTest;
+import Step.LoggingStep;
+import Step.OuraganStep;
+import Step.PaymentStep;
+import Step.TicketStep;
+import Step.Value.BaseStepValue;
+import Step.Value.Payment.PaymentStepValue;
+import io.qameta.allure.Link;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class NOE651 extends BaseTest {
+    @Parameters({"jsonFilePath", "expectedAmountToPay", "expectedState"})
+    @Test(description = "Scanner et régler en caisse une réservation Web disponible en magasin")
+    @Link(name = "Jira ticket", url = "https://openbravo.atlassian.net/browse/NOE-651")
+    public void noe651(String jsonFilePath, String expectedAmountToPay, String expectedState) throws IOException, InterruptedException {
+        // Intégration sur OB du BT Ouragan
+        String documentCode = OuraganStep.postWorkOrderToOpenBravo(jsonFilePath);
+        // Lancement et log sur OB
+        driver = LoggingStep.launchAndLogToOpenBravo();
+        // Vidage du ticket
+        TicketStep.deleteTicket(driver);
+        // Ouverture du BT Ouragan
+        OuraganStep.openWorkOrder(documentCode, driver);
+        // Vérification montant à payer
+        BaseStepValue stepValue = getNewBaseStepValue(false);
+        stepValue.expectedValue = expectedAmountToPay;
+        TicketStep.checkTotalToPay(stepValue);
+        // Vérification état commande
+        //TODO Attente C.Brazier pour accès BDD et controle etat/renvoi vers ouragan
+        stepValue.expectedValue = expectedState;
+        TicketStep.checkOrderState(stepValue);
+        // Paiement et finalisation du ticket
+        PaymentStepValue payStepValue = getNewPaymentStepValue(false);
+        payStepValue.sendTicketMode = SendTicketMode.MAIL_ONLY;
+        PaymentStep.payWithCash(payStepValue);
+        PaymentStep.finalizeOrder(payStepValue);
+        //TODO Attente C.Brazier pour accès BDD et controle etat/renvoi vers ouragan
+    }
+}
