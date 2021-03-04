c461896ace2b1305ff698745f43b27a3e4bb5f92
ValentinVANHAUTE <vvanhaute@norauto.com>
2021-03-04 11:57:41 +0100

    Augmentation timeout texte attendu

--- src/main/Step/BaseStep.java
+++ src/main/Step/BaseStep.java
@@ -2,5 +2,5 @@ package Step;
 
 public class BaseStep {
     // Timeout pour la valeur des éléments testés
-    protected static final int WAIT_FOR_VALUE_TIMEOUT_IN_SEC = 5;
+    protected static final int WAIT_FOR_VALUE_TIMEOUT_IN_SEC = 15;
 }
